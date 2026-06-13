6de048e2fc8c79bba7651d350e3cc936732c42be
Juan Delgado <judadelmal@gmail.com>
2026-06-13 18:25:46 -0500

    Quitar origin '*'

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+      origin: [
+    'http://localhost:3000',
+    'https://el-tequendama-web.vercel.app',
+    'https://eltequendama.co',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
